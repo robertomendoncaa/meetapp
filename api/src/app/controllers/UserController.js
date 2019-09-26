cd591d9f0c1d54c933d805af74f4b03f09d7b991
Roberto <robertomendonca20@gmail.com>
2019-09-26 14:09:08 -0300

    Add Middleware Authentication

--- api/src/app/controllers/UserController.js
+++ api/src/app/controllers/UserController.js
@@ -18,6 +18,12 @@ class UserController {
 
     return res.json(user);
   }
+
+  async update(req, res) {
+    console.log(req.userId);
+
+    return res.json();
+  }
 }
 
 export default new UserController();
