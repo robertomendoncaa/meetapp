754ccdea85c9b64b2c9992fd12e6866628fc929d
Roberto <robertomendonca20@gmail.com>
2019-09-26 13:51:56 -0300

    Add JWT Authentication

---
+++ api/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'cf4ea705e2790dd17d01757280b2a6ed',
+  expiresIn: '7d',
+};
--- api/src/routes.js
+++ api/src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.get('/users', UserController.index);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
