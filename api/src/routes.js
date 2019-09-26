ace1ad0b4886754dedc44837c76168d49751abd4
Roberto <robertomendonca20@gmail.com>
2019-09-26 12:55:29 -0300

    Insert first user into database

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello MeetApp' });
+routes.get('/users', async (req, res) => {
+  const user = await User.create({
+    name: 'Roberto',
+    email: 'roberto@gmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
