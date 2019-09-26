891382e787e7fbb067e8cf6ccc703d03945aeb75
Roberto <robertomendonca20@gmail.com>
2019-09-26 14:22:56 -0300

    Add Update user

--- api/src/app/controllers/UserController.js
+++ api/src/app/controllers/UserController.js
@@ -10,7 +10,11 @@ class UserController {
 
     const { id, name, email } = await User.create(req.body);
 
-    return res.json({ id, name, email });
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 
   async index(req, res) {
@@ -20,9 +24,28 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldPassword } = req.body;
 
-    return res.json();
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User email already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 }
 
